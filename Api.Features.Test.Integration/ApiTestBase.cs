723f16ebfc0df89a5166ab44a2853b6089555f79
mdelvallemurcia <mdelvallemurcia@gmail.com>
2026-02-09 12:37:43 +0100

    add integration tests

---
+++ Api.Features.Test.Integration/ApiTestBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Api.Features.Test.Integration;
+
+public class ApiTestBase : IClassFixture<WebApplicationFactory<Program>>
+{
+    protected readonly HttpClient Client;
+    protected readonly WebApplicationFactory<Program> Factory;
+
+    public ApiTestBase(WebApplicationFactory<Program> factory)
+    {
+        Factory = factory.WithWebHostBuilder(builder =>
+        {
+            // Esto le dice al test: "Busca los archivos de configuración donde está la clase Program"
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
+
+            //builder.ConfigureServices(services =>{});
+
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("appsettings.Development.json", optional: true);
+            });
+        });
+
+        Client = Factory.CreateClient();
+    }
+
+}
---
+++ Api.Features.Test.Integration/ProjectsGet/v1/Handler_Should.cs
@@ -0,0 +1,39 @@
+﻿using Api.Features.Shared.Auth;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Headers;
+
+namespace Api.Features.Test.Integration.ProjectsGet.v1;
+
+public class Handler_Should : ApiTestBase
+{
+    private readonly IBearerTokenGenerator _bearerTokenGenerator;
+
+    public Handler_Should(WebApplicationFactory<Program> factory) : base(factory)
+    {
+        var scope = factory.Services.CreateScope();
+        _bearerTokenGenerator = scope.ServiceProvider.GetRequiredService<IBearerTokenGenerator>();
+    }
+
+    [Fact]
+    public async Task ReturnUnauthorized_When_TokenIsInvalid()
+    {
+        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "invalid-token");
+
+        var response = await Client.GetAsync("/v1/projects");
+
+        Assert.NotNull(response);
+        Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+
+    [Fact]
+    public async Task ReturnOk_When_TokenIsValid()
+    {
+        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _bearerTokenGenerator.CreateToken("User"));
+
+        var response = await Client.GetAsync("/v1/projects");
+
+        Assert.NotNull(response);
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+    }
+}
